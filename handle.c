e2887af60f51e074b00f70f5889365d446e5c000
anantghuman <anant.ghuman@gmail.com>
2025-09-06 17:22:57 -0500

    started working on shell design and handle.c

--- handle.c
+++ handle.c
@@ -19,7 +19,44 @@
  */
 int main(int argc, char **argv)
 {
-  return 0;
+  pid_t pid = getpid();
+  printf("%d\n", pid);
+  init_signals();
+  struct timespec time = {1, 0};
+  while (1) {
+    ssize_t bytes;
+    const int STDOUT = 1;
+    bytes = write(STDOUT, "Nice try.\n", 10);
+    if(bytes != 10) {
+      exit(-999);
+    }
+    nanosleep(&time, NULL);
+  }
 }
+/*
+
+signal handler () {
+  set sigusr1 to sigkill function
+  set sigkill to printNiceTry function
+  }
+*/
+void init_signals() {
+  struct sigaction sa;
+  sa.sa_handler = signal_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sigaction(SIGINT, &printNiceTry, NULL);
+}
+
+void printNiceTry() {
+  const int STDOUT = 1;
+  ssize_t bytes;
+  bytes = write(STDOUT, "Nice try.\n", 10);
+  if(bytes != 10) {
+    exit(-999);
+  }
+}
+
+
 
 
